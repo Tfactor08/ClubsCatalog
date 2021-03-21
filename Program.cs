ed43a0ae0ea70a5f21260dd350decc51ca1bd43d
Tfactor08 <privetlenovo12345@gmail.com>
2021-03-21 17:57:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClubsCatalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
